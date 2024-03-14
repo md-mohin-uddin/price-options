fa206abd829e32e29f164e7a7196e411b07ceceb
Md Mohin Uddin <mdmohinuddin.cse@gmail.com>
2024-03-14 22:37:21 +0600

    Line chart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import NavBar from "./components/Navbar/NavBar";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
+import LineChart from "./LineChart/LineChart.jsx";
 // import DaisyNav from "./components/DaisyNav";
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <>
       <NavBar />
       {/* <DaisyNav /> */}
-      <h1 className="text-7xl bg-rose-400">Price Options</h1>
+      <h1 className="text-7xl bg-rose-400 text-center p-4">Price Options</h1>
       <PriceOptions />
+      <LineChart />
     </>
   );
 }
