9c13bad29754c788709312926f82123af088a157
Md Mohin Uddin <mdmohinuddin.cse@gmail.com>
2024-03-14 14:50:45 +0600

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import NavBar from "./components/Navbar/NavBar";
+// import DaisyNav from "./components/DaisyNav";
 
 function App() {
   return (
     <>
+      <NavBar />
+      {/* <DaisyNav /> */}
       <h1 className="text-7xl bg-rose-400">Price Options</h1>
     </>
   );
