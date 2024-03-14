2287082c5895c05bb8ea863acb9512dbe695f3d0
Md Mohin Uddin <mdmohinuddin.cse@gmail.com>
2024-03-14 17:44:30 +0600

    feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import NavBar from "./components/Navbar/NavBar";
+import PriceOptions from "./components/PriceOptions/PriceOptions";
 // import DaisyNav from "./components/DaisyNav";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       {/* <DaisyNav /> */}
       <h1 className="text-7xl bg-rose-400">Price Options</h1>
+      <PriceOptions />
     </>
   );
 }
